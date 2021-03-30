f952361cd742e7ddd1141d132118246f95f4c583
Letícia <leticialimaferreira2@gmail.com>
2021-03-30 14:14:52 -0300

    primeiro cenario/formulario finalizado

--- src/test/java/io/cucumber/danilo/Form1Steps.java
+++ src/test/java/io/cucumber/danilo/Form1Steps.java
@@ -21,86 +21,85 @@ public class Form1Steps {
 
 	@Dado("seleciono Model e clico em {string}")
 	public void seleciono_Model_e_clico_em(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		WebElement input = Configuracoes.browser.findElement(By.cssSelector("#model>option:nth-child(2)"));
+		input.click();
 	}
 
 	@Dado("informo em Cylinder Capacity {string}")
 	public void informo_em_Cylinder_Capacity(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		WebElement input = Configuracoes.browser.findElement(By.cssSelector("#cylindercapacity"));
+		input.sendKeys(string);
 	}
 
 	@Dado("informo em Engine Performance {string}")
 	public void informo_em_Engine_Performance(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		WebElement input = Configuracoes.browser.findElement(By.cssSelector("#engineperformance"));
+		input.sendKeys(string);
 	}
 
 	@Dado("digito em Date of Manufacture {string}")
 	public void digito_em_Date_of_Manufacture(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		WebElement input = Configuracoes.browser.findElement(By.cssSelector("#dateofmanufacture"));
+		input.sendKeys(string);
 	}
 
 	@Dado("seleciono em Number of Seats e clico em {string}")
 	public void seleciono_em_Number_of_Seats_e_clico_em(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		WebElement input = Configuracoes.browser.findElement(By.cssSelector("#numberofseats>option:nth-child(3)"));
+		input.click();		
 	}
 
 	@Dado("seleciono em Right Hand Drive e clico em {string}")
 	public void seleciono_em_Right_Hand_Drive_e_clico_em(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		WebElement input = Configuracoes.browser.findElement(By.cssSelector("#insurance-form>div>section:nth-child(1)>div:nth-child(7)>p>label:nth-child(1)>span"));
+		input.click();
 	}
 
 	@Dado("seleciono novamente Number of Seats e digito {string}")
 	public void seleciono_novamente_Number_of_Seats_e_digito(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		WebElement input = Configuracoes.browser.findElement(By.cssSelector("#numberofseatsmotorcycle>option:nth-child(3)"));
+		input.click();
 	}
 
 	@Dado("seleciono Fuel Type {string}")
 	public void seleciono_Fuel_Type(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		WebElement input = Configuracoes.browser.findElement(By.cssSelector("#fuel>option:nth-child(2)"));
+		input.click();    
 	}
 
 	@Dado("informo em Payload {string}")
 	public void informo_em_Payload(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		WebElement input = Configuracoes.browser.findElement(By.cssSelector("#payload"));
+		input.sendKeys(string);
 	}
 
 	@Dado("informo em Total Weight {string}")
 	public void informo_em_Total_Weight(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		WebElement input = Configuracoes.browser.findElement(By.cssSelector("#totalweight"));
+		input.sendKeys(string);
 	}
 
 	@Dado("informo em List Price {string}")
 	public void informo_em_List_Price(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		WebElement input = Configuracoes.browser.findElement(By.cssSelector("#listprice"));
+		input.sendKeys(string);
 	}
 
 	@Dado("informo em License Plate Number {string}")
 	public void informo_em_License_Plate_Number(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		WebElement input = Configuracoes.browser.findElement(By.cssSelector("#licenseplatenumber"));
+		input.sendKeys(string);    
 	}
 
 	@Dado("informo em Annual Mileage {string}")
 	public void informo_em_Annual_Mileage(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		WebElement input = Configuracoes.browser.findElement(By.cssSelector("#annualmileage"));
+		input.sendKeys(string);     
 	}
 
 	@Entao("devo clicar em Next para o formulario Insurant Data")
 	public void devo_clicar_em_Next_para_o_formulario_Insurant_Data() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		WebElement input = Configuracoes.browser.findElement(By.cssSelector("#nextenterinsurantdata"));
+		input.click();
 	}
-
 }
