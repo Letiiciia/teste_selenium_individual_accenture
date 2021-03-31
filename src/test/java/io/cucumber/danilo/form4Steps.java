2e6da5abcc334ab3e55cb4902508aab17257cabd
Letícia <leticialimaferreira2@gmail.com>
2021-03-30 21:32:41 -0300

    quarto formulario finalizado

---
+++ src/test/java/io/cucumber/danilo/form4Steps.java
@@ -0,0 +1,22 @@
+package io.cucumber.danilo;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import io.cucumber.danilo.servicos.Configuracoes;
+import io.cucumber.java.pt.*;
+
+
+public class form4Steps {
+	@Dado("seleciono Select Option o plano Gold")
+	public void seleciono_Select_Option_o_plano_Gold() {
+		WebElement input = Configuracoes.browser.findElement(By.cssSelector("#priceTable>tfoot>tr>th.group>label:nth-child(2)"));
+		input.click();	
+	}
+
+	@Entao("devo clicar em Next para o formulario Send Quote")
+	public void devo_clicar_em_Next_para_o_formulario_Send_Quote() throws InterruptedException{
+		Thread.sleep(5000);
+		WebElement input = Configuracoes.browser.findElement(By.cssSelector("#nextsendquote"));
+		input.click();
+	}		
+}
---
+++ src/test/resources/io/cucumber/danilo/form4.feature
@@ -0,0 +1,8 @@
+#language: pt
+Funcionalidade: Acessar os formularios da Tricentis
+  Criando uma rotina de teste para validar o site Tricentis
+
+  Cenario: Acessar e preencher o quarto formulario do site
+    Dado que eu estou no site da Trincentis no formulario "Select Price Option"
+    E seleciono Select Option o plano Gold
+    Entao devo clicar em Next para o formulario Send Quote
